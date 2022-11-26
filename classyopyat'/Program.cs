0598145f3205a3d0b3e4f05b7754e64b4ae05e0c
AntipkinD <ludewigbux@gmail.com>
2022-11-26 22:11:12 +0300

    добавил метод, выводящий страну с наибольшей плотностью населения, добавил комментарии(без излишности не обойтись), привел все в более менее презентабельный вид

--- classyopyat'/Program.cs
+++ classyopyat'/Program.cs
@@ -11,37 +11,47 @@ internal class Program
         Country country2 = new Country();
         Country country3 = new Country();
         InsertMy(country0);
-        List<object> cntrs = new List<object>(20);
-        double[] plotnost = new double[20];
-        country0.Print(country0.country, country0.capital, country0.square, country0.ppl);
+        InsertMy(country1);
+        /*InsertMy(country2);
+        InsertMy(country3);*/
+        List<string> cntrs = new List<string>(4);
+        List<double> plotnost = new List<double>(4);
         GetMassive(country0);
         GetMassive(country1);
+        /*GetMassive(country2);
+        GetMassive(country3);*/
         Perebor();
+        //GetMassive(country1);
         void GetMassive(Country value)
         {
-            cntrs.Add(value.country);
-            cntrs.Add(value.capital);
-            cntrs.Add(value.square);
-            cntrs.Add(value.ppl);
-            cntrs.Add(value.PopulationDensity(value.ppl, value.square));
-            //Perebor();
+            cntrs.Add(value.Print(value.country, value.capital, value.square, value.ppl));
+            plotnost.Add(value.PopulationDensity(value.ppl, value.square));
         }
         void Perebor()
         {
-            foreach (var cntr in cntrs)
-                Console.Write(cntr + " ");
+            foreach (var value_znach in cntrs)
+                Console.WriteLine(value_znach + "\n");
         }
         //string nazv = Console.ReadLine();
         //Country russia = new Country(Console.ReadLine(), Console.ReadLine(), double.Parse(Console.ReadLine()), ref int.Parse(Console.ReadLine()));
         void InsertMy(Country value)
         {
             Console.WriteLine("Введите название страны, ее столицы, площадь и численность населения");
+            Console.Write("Название: ");
             value.country = Console.ReadLine();
+            Console.Write("Столица: ");
             value.capital = Console.ReadLine();
+            Console.Write("Площадь: ");
             value.square = double.Parse(Console.ReadLine());
+            Console.Write("Численность: ");
             value.ppl = int.Parse(Console.ReadLine());
         }
-        
+        void MaxCountryDensity()
+        {
+            int maxIndex = plotnost.IndexOf(plotnost.Max());
+            Console.WriteLine(cntrs[maxIndex]);
+        }
+        MaxCountryDensity();
         /*InsertMy();
         InsertMy();
         InsertMy();
@@ -50,10 +60,5 @@ internal class Program
         //Console.WriteLine(Country.PPLtndnst(country.ppl, country.square));
         //$"{country.country}", $"{country.capital}", double.Parse($"{country.square}"), int.Parse($"{country.ppl}")
         //Country.Print(country.country, country.capital, country.square, country.ppl);*/
-        string znach = "5";
-        double naibolshee = 0;
-        double raznsot = naibolshee - double.Parse(znach);
-        
-        //if(raznost>0) naibolshee то же, if (raznost < 0) raznost = znach
     }
 }
